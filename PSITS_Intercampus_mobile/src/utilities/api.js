3bc1166e1667f12084eb64c956cab65122f07efd
criticalzero123 <jdcaramonte159@gmail.com>
2023-03-31 17:22:15 +0800

    Add: splash screen and logo

--- PSITS_Intercampus_mobile/src/utilities/api.js
+++ PSITS_Intercampus_mobile/src/utilities/api.js
@@ -1,8 +1,8 @@
 import md5 from "md5";
 
-let baseUrl = "http://119.92.196.92:3000";
-
-export const accessKey = md5("PSITS2023");
+let baseUrl = "API_HERE";
+let key = "KEY HERE";
+export const accessKey = md5(key);
 
 export const _fetch = (url, method, options) => {
   return fetch(`${baseUrl}${url}`, {
