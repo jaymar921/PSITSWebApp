099935afeaa759e4438597239fea56a4b35837c7
criticalzero123 <jdcaramonte159@gmail.com>
2023-05-09 22:27:26 +0800

    Fix: api url http

--- PSITS_Intercampus_mobile/src/utilities/api.js
+++ PSITS_Intercampus_mobile/src/utilities/api.js
@@ -7,8 +7,8 @@ export const accessKey = md5(key);
 export const _fetch = async (url, method, options) => {
   const ipadd = await AsyncStorage.getItem("ipadd");
   const port = await AsyncStorage.getItem("port");
-  console.log(`${ipadd}:${port}${url}`);
-  return fetch(`${ipadd}:${port}${url}`, {
+
+  return fetch(`http://${ipadd}:${port}${url}`, {
     method: method.toUpperCase(),
     headers: options.headers,
   });
